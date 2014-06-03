261c49961b97e3a2b16ea9d4921166ca5acd9930
arnif <arnifa@gmail.com>
2014-06-03 23:30:05 +0000

    fixed tests

--- test/client/spec/controllers/game.js
+++ test/client/spec/controllers/game.js
@@ -3,7 +3,7 @@
 describe('Controller: GameCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('yoMeanApp'));
+  beforeEach(module('yoEloApp'));
 
   var GameCtrl,
     scope;
